e867ae138b063c5bd38da3488fdf70b17f21ca7d
zhangyule <zyl2336709@gmail.com>
2018-05-05 20:48:07 +0800

    update

---
+++ inc/detail/context_impl.hpp
@@ -0,0 +1,19 @@
+class Context {
+public:
+  template <typename T>
+  T* cast() {
+    return dynamic_cast<T*>(this);
+  }
+
+  void register(const std::string& name, T* t) {
+    this->_context[name] = dynamic_cast<T*>(t);
+  }
+
+  Context* call(const std::string& name) {
+    return _context->at(name);
+  }
+
+private:
+  std::unordered_map<std::string, Context*> _context;
+
+};
---
+++ inc/detail/node_def.hpp
@@ -0,0 +1,26 @@
+template <typename Key, typename TagType>
+struct NodeType {
+  typedef Key key_type;
+  typedef TagType tag_type;
+  Key key;
+  bool mark;
+  NodeType<Key, TagType>* next;
+  TagType tag;
+};
+
+template <typename NType>
+struct MarkPtrType {
+  typedef NType node_type;
+  bool mark;
+  NType* next;
+  typename NType::tag tag;
+};
+
+
+// template MarkPtrType
+template <typename MPType, int M>
+struct Context {
+  typedef MPType mark_ptr_type;
+  enum { mark_ptr_num = M };
+  MPType mark_ptrs[M];
+};
